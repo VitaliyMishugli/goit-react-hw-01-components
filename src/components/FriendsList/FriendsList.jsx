04b3a8ad117e38231361fefe27e09b1fa1817d05
Vitaliy Mishugli <vmishughli@gmail.com>
2022-10-23 18:57:04 +0100

    commit

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
-import { FriendsListRender, FriendsListItem, FriendsSection, FriendsSpan } from './FriendsList.styled';
+import { FriendsListRender, FriendsListItem,  FriendsSpan } from './FriendsList.styled';
 
 const FriendsList = ({friends}) => {
   return   <FriendsListRender>
     {
       (friends.map(friend => {
-         console.log(friend.isOnline);
+        //  console.log(friend.isOnline);
         return <FriendsListItem key={friend.id} >
           <FriendsSpan online={friend.isOnline}></FriendsSpan>
          
--- src/components/FriendsList/FriendsList.styled.jsx
+++ src/components/FriendsList/FriendsList.styled.jsx
@@ -1,19 +1,19 @@
 import styled from '@emotion/styled';
 
-export const FriendsSection = styled.section`
-  display: block;
-  width: 460px;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 50px;
-  // margin-bottom: var(--ifm-leading);
-  // max-width: 480px;
-   background-color: #fff;
-  border: 1px solid #434545;
-  border-radius: 6px;
-  box-shadow: 5px 5px 10px #888888;
-  padding: 10px;
-`;
+// export const FriendsSection = styled.section`
+//   display: block;
+//   width: 460px;
+//   margin-left: auto;
+//   margin-right: auto;
+//   margin-top: 50px;
+//   // margin-bottom: var(--ifm-leading);
+//   // max-width: 480px;
+//    background-color: #fff;
+//   border: 1px solid #434545;
+//   border-radius: 6px;
+//   box-shadow: 5px 5px 10px #888888;
+//   padding: 10px;
+// `;
 
 
 export const FriendsListRender = styled.ul`
