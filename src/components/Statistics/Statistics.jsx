b6abfe0bb0b31e5a9b311e8c8b63199de8bffda2
Vitaliy Mishugli <vmishughli@gmail.com>
2022-10-24 21:32:35 +0100

    fix/add isRequired

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -13,23 +13,8 @@ const Statistics = ({title, stats}) => {
            <span >{ stat.percentage}%</span>
         </StatisticItem>
       ))}
-    {/* <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
-    </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
-    </li> */}
-  </StatisticList>
+    </StatisticList>
+    
 </StatisticCard>
   
 }
