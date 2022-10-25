afbc5dd576455815a9568618adfb2c1b0f02485f
Vitaliy Mishugli <vmishughli@gmail.com>
2022-10-25 19:58:48 +0100

    Update FriendsList.jsx

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import friends from "../FriendsList/friends.json";
 import FriendsListItem from './FriendListItem/FriendListItem';
 
